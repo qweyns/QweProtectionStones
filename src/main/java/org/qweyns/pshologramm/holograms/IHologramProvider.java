f3ad0e7ae916ba3c95e8bb2e047b564a3b0a57a6
qweyns <kartonka845@gmail.com>
2026-07-29 22:06:13 +0700

    Delete src/main/java/org/qweyns/pshologramm directory

--- src/main/java/org/qweyns/pshologramm/holograms/IHologramProvider.java
+++
@@ -1,9 +0,0 @@
-package org.qweyns.pshologramm.holograms;
-
-import org.bukkit.Location;
-
-public interface IHologramProvider {
-    void createOrUpdate(String regionId, Location loc, String regionType, String owner, int dur, int maxDur);
-    void remove(String regionId);
-    void deleteAll();
-}
